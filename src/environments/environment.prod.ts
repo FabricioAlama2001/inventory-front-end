eec401222d207f1c4a04e74a41daf2c9a717576c
Cesar Mauricio Tamayo <cesar.tamayo@turismo.gob.ec>
2024-03-28 16:49:46 -0500

    change api url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  API_URL: 'https://siplaneo.yavirac.dev:8443/api/v1',
-  HOST_URL: 'https://siplaneo.yavirac.dev:8443/',
+  API_URL: 'https://api.siplaneo.yavirac.dev/api/v1',
+  HOST_URL: 'https://api.siplaneo.yavirac.dev/',
   APP_NAME:'Sistema de Planificación - Siplaneo',
   APP_SHORT_NAME:'SiPlaneo',
 };
