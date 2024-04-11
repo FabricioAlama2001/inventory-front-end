0a1283c92a8466aacf77c845cdc66a1d7f62363e
FabricioAlama2001 <fabricioalama2001@outlook.es>
2024-04-11 11:24:50 -0500

    update proyect

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  API_URL: 'https://api.siplaneo.yavirac.dev/api/v1',
-  HOST_URL: 'https://api.siplaneo.yavirac.dev/',
+  API_URL: 'https://api.inventory.amauta.education/api/v1',
+  HOST_URL: 'https://api.inventory.amauta.education/',
   APP_NAME:'Sistema de inventario',
   APP_SHORT_NAME:'SICNM',
 };
